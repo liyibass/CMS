c2e35d31c2af8889f31ed0818d1b1f3bd5f0093a
“liyibass” <liyibass@mirrormedia.mg>
2020-11-27 12:38:13 +0800

    build: project init.

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NuxtApp } = require('@keystonejs/app-nuxt');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'CMS';
+const adapterConfig = { mongoUri: 'mongodb+srv://liyi:0425@cms01.tpi0o.mongodb.net/test' };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ name: PROJECT_NAME }),
+    new NuxtApp({
+      srcDir: 'src',
+      buildDir: 'dist',
+    }),
+  ],
+};
